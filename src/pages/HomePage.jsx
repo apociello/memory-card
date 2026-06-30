64362302fdd8a3ca0ada48d3fb6b02ec2c0ea0d1
pociello <asierpociello@gmail.com>
2026-06-30 12:36:59 +0200

    feat: add & positon minecraft iconic title

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,13 @@ export default function HomePage() {
   return (
     <>
       <main className="home">
-        <h1>MINECRAFT MEMORY</h1>
-        <button>Play</button>
+        <div className="title-wrapper">
+          <img src="/title1.png" className="title1" alt="" />
+          <img src="/title2.png" className="title2" alt="" />
+        </div>
+        <div className="button-wrapper">
+          <button>Play</button>
+        </div>
       </main>
       <footer>
         <button>Sound</button>
