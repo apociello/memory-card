87cb496f0e142f7e58028bf2cdc830306fe78d23
pociello <asierpociello@gmail.com>
2026-06-30 09:14:35 +0200

    feat: add card images & create data objects array

---
+++ src/data/data.js
@@ -0,0 +1,103 @@
+export const cardList = [
+  {
+    id: 1,
+    name: 'Bow',
+    img: 'Bow.png',
+  },
+  {
+    id: 2,
+    name: 'Cake',
+    img: 'Cake.webp',
+  },
+  {
+    id: 3,
+    name: 'Carrot',
+    img: 'Carrot.png',
+  },
+  {
+    id: 4,
+    name: 'Chest',
+    img: 'Chest.png',
+  },
+  {
+    id: 5,
+    name: 'Crafting Table',
+    img: 'Crafting_Table.png',
+  },
+  {
+    id: 6,
+    name: 'Creeper Head',
+    img: 'Creeper_Head.png',
+  },
+  {
+    id: 7,
+    name: 'Diamond Chestplate',
+    img: 'Diamond_Chestplate.webp',
+  },
+  {
+    id: 8,
+    name: 'Diamond',
+    img: 'Diamond.png',
+  },
+  {
+    id: 9,
+    name: 'Emerald',
+    img: 'Emerald.png',
+  },
+  {
+    id: 10,
+    name: 'Ender Pearl',
+    img: 'Ender_Pearl.png',
+  },
+  {
+    id: 11,
+    name: 'Eye of Ender',
+    img: 'Eye_of_Ender.png',
+  },
+
+  {
+    id: 12,
+    name: 'Golden Apple',
+    img: 'Golden_Apple.png',
+  },
+  {
+    id: 13,
+    name: 'Grass Block',
+    img: 'Grass_Block.png',
+  },
+  {
+    id: 14,
+    name: 'Iron Pickaxe',
+    img: 'Iron_Pickaxe.png',
+  },
+  {
+    id: 15,
+    name: 'Oak Log',
+    img: 'Oak_Log.png',
+  },
+  {
+    id: 16,
+    name: 'Red Bed',
+    img: 'Red_Bed.webp',
+  },
+  {
+    id: 17,
+    name: 'Sponge',
+    img: 'Sponge.png',
+  },
+  {
+    id: 18,
+    name: 'TNT',
+    img: 'TNT.png',
+  },
+  {
+    id: 19,
+    name: 'Totem',
+    img: 'Totem.png',
+  },
+  {
+    id: 20,
+    name: 'Wooden Axe',
+    img: 'Wooden_Axe.png',
+  },
+];
