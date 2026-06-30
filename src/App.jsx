420c57ab775bdebf9b21c8cc278edf60675be092
pociello <asierpociello@gmail.com>
2026-06-30 10:36:59 +0200

    feat: create homePage layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import HomePage from './pages/HomePage';
 
 export default function App() {
   return (
-    <video autoPlay loop muted playsInline className="bg-video">
-      <source src="/fox.mp4" type="video/mp4" />
-    </video>
+    <>
+      <video autoPlay loop muted playsInline className="bg-video">
+        <source src="/fox.mp4" type="video/mp4" />
+      </video>
+      
+      <HomePage />
+    </>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import '../styles/HomePage.css';
+
+export default function HomePage() {
+  return (
+    <>
+      <main className="home">
+        <h1>MINECRAFT MEMORY</h1>
+        <button>Play</button>
+      </main>
+      <footer>
+        <button>Sound</button>
+        <button>Music</button>
+      </footer>
+    </>
+  );
+}
