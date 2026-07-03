4ca3f02e09d13c0a91d60864d037e1930faa7851
pociello <asierpociello@gmail.com>
2026-07-03 16:37:34 +0200

    feat: create rounds incrementing difficulty logic & reset when game over

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -3,6 +3,9 @@ import Card from '../components/Card';
 import cardList from '../data/data.js';
 import '../styles/GamePage.css';
 
+const INITIAL_CARDS = 4;
+const CARD_INCREMENT = 2;
+
 function getRandomCards(numCards) {
   const newList = [];
 
@@ -28,25 +31,45 @@ function shuffleCards(array) {
 export default function GamePage({ playGame, playClick }) {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [currentCards, setCurrentCards] = useState(() => getRandomCards(4));
+  const [currentCards, setCurrentCards] = useState(() =>
+    getRandomCards(INITIAL_CARDS),
+  );
   const [chosenCardIds, setChosenCardIds] = useState([]);
+
+  function nextRound(newScore) {
+    if (newScore === cardList.length) {
+      console.log('You won!');
+    } else {
+      setCurrentCards(getRandomCards(currentCards.length + CARD_INCREMENT));
+      setChosenCardIds([]);
+      console.log('next round');
+    }
+  }
+
+  function resetGame() {
+    setScore(0);
+    setChosenCardIds([]);
+    setCurrentCards(getRandomCards(INITIAL_CARDS));
+  }
 
   function pickCard(cardId) {
     const alreadyChosen = chosenCardIds.includes(cardId);
 
     if (alreadyChosen) {
+      resetGame();
       console.log('GAME OVER!');
-      setScore(0);
-      setChosenCardIds([]);
     } else {
       const newScore = score + 1;
       setScore(newScore);
       if (newScore > bestScore) setBestScore(newScore);
       setChosenCardIds([...chosenCardIds, cardId]);
-      if (newScore === currentCards.length) console.log('You won!');
-    }
 
-    setCurrentCards(shuffleCards(currentCards));
+      if (chosenCardIds.length + 1 === currentCards.length) {
+        nextRound(newScore);
+      } else {
+        setCurrentCards(shuffleCards(currentCards));
+      }
+    }
   }
 
   return (
