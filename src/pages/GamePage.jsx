de9db6c9971d3fcd34dd4c8a7462e6929b452b01
pociello <asierpociello@gmail.com>
2026-07-03 13:01:39 +0200

    refactor: improve gamePage readability & performance

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -3,63 +3,47 @@ import Card from '../components/Card';
 import cardList from '../data/data.js';
 import '../styles/GamePage.css';
 
+function getRandomCards(numCards) {
+  const newList = [];
+
+  while (newList.length < numCards) {
+    const chosenIndex = Math.floor(Math.random() * cardList.length);
+    const chosenCard = cardList[chosenIndex];
+    const alreadyPicked = newList.some((card) => card.id === chosenCard.id);
+    if (!alreadyPicked) newList.push(chosenCard);
+  }
+
+  return newList;
+}
+
+function shuffleCards(array) {
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
 export default function GamePage({ playGame, playClick }) {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [currentCards, setCurrentCards] = useState(getRandomCards(4));
+  const [currentCards, setCurrentCards] = useState(() => getRandomCards(4));
   const [chosenCardIds, setChosenCardIds] = useState([]);
 
-  function getRandomCards(numCards) {
-    const newList = [];
-
-    for (let i = 0; i < numCards; i++) {
-      let repeated = 1;
-
-      while (repeated) {
-        repeated = 0;
-        let chosenIndex = Math.floor(Math.random() * cardList.length);
-
-        for (let j = 0; j < newList.length && !repeated; j++) {
-          if (newList[j].id === cardList[chosenIndex].id) {
-            repeated = 1;
-          }
-        }
-
-        if (!repeated) newList.push(cardList[chosenIndex]);
-      }
-    }
-
-    return newList;
-  }
-
-  function shuffleCards(array) {
-    const arr = [...array];
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-  }
-
   function pickCard(cardId) {
-    let repeated = 0;
+    const alreadyChosen = chosenCardIds.includes(cardId);
 
-    for (let i = 0; i < chosenCardIds.length && !repeated; i++) {
-      if (chosenCardIds[i] === cardId) {
-        console.log('GAME OVER!');
-        setScore(0);
-        setChosenCardIds([]);
-        repeated = 1;
-      }
-    }
-
-    if (!repeated) {
-      setScore(score + 1);
-      if (score + 1 > bestScore) setBestScore(bestScore + 1);
-      const newArray = [...chosenCardIds];
-      newArray.push(cardId);
-      setChosenCardIds(newArray);
-      if (score + 1 === currentCards.length) console.log('You won!');
+    if (alreadyChosen) {
+      console.log('GAME OVER!');
+      setScore(0);
+      setChosenCardIds([]);
+    } else {
+      const newScore = score + 1;
+      setScore(newScore);
+      if (newScore > bestScore) setBestScore(newScore);
+      setChosenCardIds([...chosenCardIds, cardId]);
+      if (newScore === currentCards.length) console.log('You won!');
     }
 
     setCurrentCards(shuffleCards(currentCards));
